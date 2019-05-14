d4a672e607a7e0172145f23dd5c57fdfab16abc0
Ignacio Alonso <nacho.allibre@gmail.com>
2019-05-13 22:59:47 -0500

    Change forms from ajax to ActionCable

--- README.md
+++ README.md
@@ -1,24 +1,18 @@
-# README
+# Chat
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+## Running 
 
-Things you may want to cover:
+- Ruby 2.5.3
+- Rails 5.2.3
 
-* Ruby version
 
-* System dependencies
+## Brief description
 
-* Configuration
+When you press enter the message is sent via the websocket connection
+It lands in the right channel and gets processed and saved in the database then its sent to the messageRelayJob which triggers my background workers which talk to the actioncable redis connection 
+that sends it out to every subscriber and then it gets received and displayed in the browser.
 
-* Database creation
 
-* Database initialization
+## Weaknesses 
 
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+If actioncable is down the web app will not save any messages since I'm only using ActionCable to create the messages.
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,21 +1,27 @@
 class MessagesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_chatroom
 
-  def create
-    message = @chatroom.messages.new(message_params)
-    message.user = current_user
-    message.save
-    MessageRelayJob.perform_later(message)
-  end
+  # This controller is not needed anymore because
+  # We are no longer using ajax to save our messages
+  # We are using ActionCable on chatrooms_channel
 
-  private 
+  
+  # before_action :authenticate_user!
+  # before_action :set_chatroom
 
-  def set_chatroom
-    @chatroom = Chatroom.find(params[:chatroom_id])
-  end
+  # def create
+  #   message = @chatroom.messages.new(message_params)
+  #   message.user = current_user
+  #   message.save
+  #   MessageRelayJob.perform_later(message)
+  # end
 
-  def message_params
-    params.require(:message).permit(:body)
-  end
+  # private 
+
+  # def set_chatroom
+  #   @chatroom = Chatroom.find(params[:chatroom_id])
+  # end
+
+  # def message_params
+  #   params.require(:message).permit(:body)
+  # end
 end
